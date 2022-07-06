bd316aa102eda8e1eff55e96e40261489c4fd8cd
receptas <50878750+recept27@users.noreply.github.com>
2022-07-06 19:06:16 +0300

    Update yacht-5.js

--- pages/tekneler/yacht-5.js
+++ pages/tekneler/yacht-5.js
@@ -1,14 +1,1 @@
-import BoatPage from "../../page-templates/BoatPage";
 
-export function getStaticProps({ locale }) {
-  "Current Locale", locale;
-  return {
-    props: {
-      messages: require(`../../locales/${locale}.json`),
-    },
-  };
-}
-
-const BogazCocugu = () => <BoatPage boatUrl="yacht-5" />;
-
-export default BogazCocugu;
